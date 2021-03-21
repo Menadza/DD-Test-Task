e620822ed4ec979d45985969259a35a5c80198a3
Denis <menadza000@gmail.com>
2021-03-21 19:37:46 +0300

    create main class, add file reader\writer

---
+++ src/java/com/kondratev/init/solution/Main.java
@@ -0,0 +1,20 @@
+package com.kondratev.init.solution;
+
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try (BufferedReader reader = new BufferedReader(new FileReader("inputText.txt"));
+         BufferedWriter writer = new BufferedWriter(new FileWriter("outputText.txt"))) {
+      String tempLine;
+      LineExtractor lineExtractor;
+      while ((tempLine = reader.readLine()) != null) {
+        lineExtractor = new LineExtractor(tempLine);
+        writer.write(lineExtractor.transform()+"\n");
+      }
+      System.out.println("All complete.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
